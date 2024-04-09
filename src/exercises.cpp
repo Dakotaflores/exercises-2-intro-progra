61f782d7bb7b872022145dcb0db9d2eabceb3e87
Dakotaflores <143897722+Dakotaflores@users.noreply.github.com>
2024-04-09 00:02:37 -0400

    comi789

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -33,16 +33,16 @@ void exercise_2(string s1) {
 void exercise_3(string s1) {
   // TODO: YOUR CODE HERE
 
-    getline(cin, s1);
+    //getline(cin, s1);
     
     for(int i = 0 ; i < s1.size() ; ++i) {
         
         if(i >= 10) {
             break;
         } else if (s1[i] == '1') {
-            cout << "Om-nom-nom :P\n";
+            cout << "Om-nom-nom :P" << endl;
         } else if (s1[i] == '0') {
-            cout << "No cake :(\n"; 
+            cout << "No cake :(" << endl; 
             break;
         }
         
