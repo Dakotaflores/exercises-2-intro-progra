3ce307e3dd36663981d22a0dab5916b3aec70b9a
Dakotaflores <143897722+Dakotaflores@users.noreply.github.com>
2024-04-09 00:04:00 -0400

    youcomit89

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -37,7 +37,7 @@ void exercise_3(string s1) {
     
     for(int i = 0 ; i < s1.size() ; ++i) {
         
-        if(i >= 10) {
+        if(i >= 20) {
             break;
         } else if (s1[i] == '1') {
             cout << "Om-nom-nom :P" << endl;
