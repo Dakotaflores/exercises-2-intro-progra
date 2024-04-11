34512f35436a31161465ecbe234c2594586f4c01
Dakotaflores <143897722+Dakotaflores@users.noreply.github.com>
2024-04-10 22:18:28 -0400

    inge90

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -91,9 +91,6 @@ void exercise_5(int n, int k) {
 int exercise_6(int n) {
   // TODO: YOUR CODE HERE
 
-  cout << "Ingrese un numero: ";
-  cin >> n;
-
   if (n < 0) {
     cout << "Numero no valido";
     }
