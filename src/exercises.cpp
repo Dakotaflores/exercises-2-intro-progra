aef5ab0cf07b3430d2756e40f279081787687ca7
Dakotaflores <143897722+Dakotaflores@users.noreply.github.com>
2024-04-10 22:17:52 -0400

    using789

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -90,6 +90,21 @@ void exercise_5(int n, int k) {
 
 int exercise_6(int n) {
   // TODO: YOUR CODE HERE
+
+  cout << "Ingrese un numero: ";
+  cin >> n;
+
+  if (n < 0) {
+    cout << "Numero no valido";
+    }
+
+    int suma = 0;
+    while (n > 0) {
+        suma = suma + e % 10; 
+        n = n / 10; 
+      }
+      cout << "El resultado de la suma es: " << suma;
+
   return 0;
 }
 
